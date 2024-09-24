bb4f692e39a09a8d6a2afe43cd2bf963fa27e3f2
anuj <anujsbhuyar@gmail.com>
2024-09-24 08:25:09 +0530

    init added additional components

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import ContactSection from "@/components/ContactSection";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import { BackgroundCellAnimation } from "@/components/Hero";
+import { FloatingNavDemo } from "@/components/Navbar";
+import Testimonials from "@/components/Testimonials";
+
+export default function Home() {
+  return (
+    <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <FloatingNavDemo />
+      <BackgroundCellAnimation />
+      <Features />
+      <Testimonials />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
